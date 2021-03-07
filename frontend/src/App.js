f697fa3f2389baeaa41c637a5f413d6e7a740e11
rryaboshtan <rrvworkroman@gmail.com>
2021-03-07 19:25:26 +0200

    Moving HTML elements Rating and Product card to React Components Rating and Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,77 +2,45 @@ import React from 'react';
 // import logo from './logo.svg';
 import './index.css';
 import data from "./data";
+import Product from './components/Product';
 
 function App() {
-  return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-              <button>
-                 &#9776;
-              </button>
-          <a className="brand" href="/"> 
-            amazon 
-          </a>
-        </div>
+    return (
+        <div className="grid-container">
+            <header className="row">
+                <div className="brand">
+                    <button>
+                        &#9776;
+                    </button>
+                    <a  href="/">
+                        amazon
+                    </a>
+                </div>
 
-        <div className="header-links">
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
+                <div className="header-links">
+                    <a href="/cart">Cart   </a>
+                    <a href="/signin">  Sign In</a>
 
-        </div>
-      </header>
-      <main className="main">
-        <div className="row center">
-          { data.products.map((product) =>(
+                </div>
+            </header>
+            <main className="main">
+                <div className="row center">
+                    {data.products.map((product) => (
 
-          
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img  
-                  className="medium" 
-                  src={product.image} 
-                  alt="product"/>
-              </a>
+                        <Product key={product._id} product={product}> </Product>
+                        
+                    ))
+                    }
 
-              <div className="card-body">
-                <a href={`/product/${product._id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-              </div>
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star-o"></i>
-                </span>
-              </div>
-              <div className="price">
-                ${product.price}
-              </div>
-            </div>
-            ))
-          }
-           
-        </div>
-      </main>
-      
-                  <footer className="flexCentered">
-                    All rights reserved.
+                </div>
+            </main>
+
+            <footer className="flexCentered">
+                All rights reserved.
                   </footer>
-      </div>
-       
-  );
+        </div>
+
+    );
 }
 
 export default App;
